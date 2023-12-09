1c290e367110c3ee5189339f51909fa359658bad
Ihor Prochnickiy <2prochigor@gmail.com>
2023-12-09 19:04:09 +0200

    refactor files with black and add train to settings

--- train/apps.py
+++ train/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TrainConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'train'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "train"
